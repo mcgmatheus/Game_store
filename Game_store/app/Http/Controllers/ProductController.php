9bda0c61a4bf09939e9cd7cdf406c583e383f3f8
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-19 12:31:16 -0300

    Cria rotas, model e controller

---
+++ Game_store/app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){}
+    public function store(){}
+    public function show(){}
+    public function update(){}
+    public function destroy(){}
+}
