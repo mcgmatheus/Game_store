a76581eb3a78fdd8899e14cd6269e287e874563f
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-20 16:36:07 -0300

    Cria rotas, model e controller de avaliations

---
+++ Game_store/app/Http/Controllers/AvaliationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AvaliationController extends Controller
+{
+    public function store(){}
+    public function index(){}
+    public function update(){}
+    public function destroy(){}
+}
