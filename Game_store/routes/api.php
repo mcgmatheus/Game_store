3e21380812027988d66844c5064c645ec01aa0b7
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-19 23:48:32 -0300

    feat: cadastro, listagem e exclusao de marcadores
    Cria rotas, model e controller de markers

--- Game_store/routes/api.php
+++ Game_store/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MarkerController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,7 @@ Route::get('products',[ProductController::class,'index']);
 Route::get('products/{id}',[ProductController::class,'show']);
 Route::put('products/{id}',[ProductController::class,'update']);
 Route::delete('products/{id}',[ProductController::class,'destroy']);
+
+Route::post('markers', [MarkerController::class, 'store']);
+Route::get('markers', [MarkerController::class, 'index']);
+Route::delete('markers/{id}', [MarkerController::class, 'destroy']);
