e6d57ff5ff6c5f1db4bda46ddadadd6e284afbfe
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-20 15:02:57 -0300

    feat: cadastro, listagem e exclusao de marcadores de jogos
    Cria rotas, model e controller para cadastrar marcadores à um jogo especifico, listar todos os marcadores de um jogo e deletar marcadores de um jogo.

--- Game_store/routes/api.php
+++ Game_store/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MarkerController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductMarkerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('products',[ProductController::class,'store']);
@@ -13,3 +14,7 @@ Route::delete('products/{id}',[ProductController::class,'destroy']);
 Route::post('markers', [MarkerController::class, 'store']);
 Route::get('markers', [MarkerController::class, 'index']);
 Route::delete('markers/{id}', [MarkerController::class, 'destroy']);
+
+Route::post('product_markers', [ProductMarkerController::class, 'store']);
+Route::get('product_markers', [ProductMarkerController::class, 'index']);
+Route::delete('product_markers/{id}', [ProductMarkerController::class, 'destroy']);
