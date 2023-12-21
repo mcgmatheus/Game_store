f609e03ceec5f60a5828ce20f8ee3c418088e7bb
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-12-21 15:16:31 -0300

    Cria rotas, model e controller de categories

---
+++ Game_store/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function store(){}
+    public function index(){}
+    public function update(){}
+    public function destroy(){}
+
+}
